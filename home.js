aad8e609845481224b288a0035c5a11307164d02
Lizzy Wiley <Lizzyrho@gmail.com>
2021-07-01 14:31:32 -0500

    Added functions to javascript

--- home.js
+++ home.js
@@ -1,4 +1,17 @@
 alert("Welcome");
-if (confirm("are you okay?")) {txt = "Good!";} else {txt = "OK";}
-var userName= prompt("What is your name?");
-console.log(userName);
+if (confirm("are you okay?")) {
+  var txt = "Good!";
+  alert(txt);
+  console.log(txt);
+} else {
+   var txt = "OK";
+   console.log(txt);
+}
+
+var userName = prompt("What is your name?");
+alert("Welcome " + userName);
+if (userName == null) {
+    alert("Please enter your name");
+}
+
+
