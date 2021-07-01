c9864ce20f48d0132d0c54a81571548a5a874165
Lizzy Wiley <Lizzyrho@gmail.com>
2021-07-01 18:01:07 -0500

    Added for a while loop

--- home.js
+++ home.js
@@ -10,8 +10,26 @@ if (confirm("are you okay?")) {
 
 var userName = prompt("What is your name?");
 alert("Welcome " + userName);
-if (userName == null) {
+if (userName == "") {
     alert("Please enter your name");
 }
+
+var hungry = true;
+var question;
+
+while(hungry==true){
+  question = prompt("Are you hungry?, Yes or no?")
+  if (question =="yes") {hungry = false;} 
+}
+
+var scale = prompt("What do you rate us, 1-5?");
+for(let i =0; i<scale; i++) {
+  document.write("<img src='Soul.jpg'");
+}
+
+
+
+
+
 
 
